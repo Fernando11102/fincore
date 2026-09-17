33b44c192f3f2b55fabc2c8ccd2f122f7c2b616e
Fernando Pinanjota <pinanjotaf7@gmail.com>
2026-09-17 20:12:38 +0100

    feat: add account lookup endpoint with 404 handling

--- src/main/java/com/fernando/fincore/account/AccountController.java
+++ src/main/java/com/fernando/fincore/account/AccountController.java
@@ -1,8 +1,11 @@
 package com.fernando.fincore.account;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -20,5 +23,19 @@ public class AccountController {
                 new Account("FC100002", AccountType.SAVINGS);
 
         return List.of(currentAccount, savingsAccount);
+    }
+    @GetMapping("/{accountNumber}")
+    public Account getAccount(@PathVariable String accountNumber) {
+
+        return getAccounts()
+                .stream()
+                .filter(account -> account.getAccountNumber().equals(accountNumber))
+                .findFirst()
+                .orElseThrow(() ->
+                        new ResponseStatusException(
+                                HttpStatus.NOT_FOUND,
+                                "Account not found"
+                        )
+                );
     }
 }
